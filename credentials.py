9a89a51d4e3cf9e829617ce9c07a38af47a79d5e
Pedritos22 <pietromolendys@gmail.com>
2025-04-11 16:03:45 +0200

    feat: Add function to load credentials from a file

---
+++ credentials.py
@@ -0,0 +1,13 @@
+import yaml
+import logging
+
+def load_credentials(filepath):
+    try:
+        with open(filepath, 'r') as file:
+            credentials = yaml.safe_load(file)
+            user = credentials['user']
+            password = credentials['password']
+            return user, password
+    except Exception as e:
+        logging.error("Failed to load credentials: {}".format(e))
+        raise
